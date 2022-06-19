4e1b5d5c831818147e598b3e1fa69831d7b33cf8
Aleksandr Demidenko <sashatro@mail.ru>
2022-06-19 16:57:04 +0300

    Add models

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+//const mongoose = require('mongoose');
+const path = require('path');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//     useFindAndModify: false
+// });
+
+//app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
