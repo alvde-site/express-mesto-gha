d2cf6c2afb747a7aace92fad80db68b42acff265
Aleksandr Demidenko <sashatro@mail.ru>
2022-06-20 13:11:42 +0300

    Handle /likes routes

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,3 +26,27 @@ module.exports.createCard = (req, res) => {
       res.status(500).send({ message: "Произошла ошибка", error: err })
     );
 };
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true }
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) =>
+      res.status(500).send({ message: "Произошла ошибка", error: err })
+    );
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true }
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) =>
+      res.status(500).send({ message: "Произошла ошибка", error: err })
+    );
+};
