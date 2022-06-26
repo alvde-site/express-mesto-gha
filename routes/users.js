6b227036dd8e7d6e8043756729b91bb2eed50baf
Aleksandr Demidenko <sashatro@mail.ru>
2022-06-26 16:45:18 +0300

    Добавить hex в роут users

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ usersRouter.get('/', getUsers);
 usersRouter.get('/:userId', celebrate({
   // валидируем параметры
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    userId: Joi.string().length(24).hex().required(),
   }),
 }), getUserById);
 
